800dacfb221c4d279aaf60519fc641492fbb6c59
jlee66 <jlee66@babson.edu>
2017-07-07 22:38:09 +0200

    Update airbnb_data.R

--- R/airbnb_data.R
+++ R/airbnb_data.R
@@ -1,14 +1,18 @@
 #' This is data to be included in my package
 #'
+#' The Data has 53940 observations, which include 10 variables such as 
+#' neighborhood, price, bedrooms, longtitude/latitude, etc. The currency of
+#' the price is in US dollars. 
+#'
 #' @name amsterdam
-
+#'
 #' @docType data
-
+#'
 #' @keywords data
-
+#'
 #' @usage data(amsterdam)
-
+#'
 #' @source \href{http://tomslee.net/airbnb-data-collection-get-the-data}{QTL Archive}
-
+#'
 #' @format A data frame with 53940 rows and 10 variables
 NULL
