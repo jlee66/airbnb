409c1a821367c3ea8ca2b6847a8a22e633845b15
jlee66 <jlee66@babson.edu>
2017-07-07 22:26:21 +0200

    Update airbnb_data.R

--- R/airbnb_data.R
+++ R/airbnb_data.R
@@ -1,8 +1,14 @@
 #' This is data to be included in my package
 #'
 #' @name amsterdam
+
 #' @docType data
+
 #' @keywords data
+
 #' @usage data(amsterdam)
+
+#' @source \href{http://tomslee.net/airbnb-data-collection-get-the-data}{QTL Archive}
+
 #' @format A data frame with 53940 rows and 10 variables
 NULL
